cf5ba079f5eb6d08ce498f628f58c574763c6551
huhuhuHR <757033301@qq.com>
2017-10-10 23:34:49 +0800

    feat(blog):css优化

--- src/pages/blog/Article/ActicleList.vue
+++ src/pages/blog/Article/ActicleList.vue
@@ -3,24 +3,16 @@
     <goBack @goBack="goBack"
             @goHome="goHome"></goBack>
     <div class="add" @click="goAdd()">添加新的文章:<i class="iconfont icon-icon02"></i></div>
-    <div style="border-top: 2px solid #8c8c8c;margin: 0 30px 0 30px;"></div>
-    <div class="article-list" v-for="(article,index) in personRecordList" key="article.id"
-         @click="goDetail(article.id)">
-      <div class="article-title">标题:{{article.title}}</div>
-      <!--<div :class="{'toggleShow':toggleShowTag[index],'article-body':true}" v-html="article.body"></div>-->
-      <div class="article-time">日期：{{article.createTime | dataFilter}}</div>
-      <div class="last-line">
-        <!--<div class="hidden" @click.stop="toggle(index)">-->
-        <!--<div v-show="toggleShowTag[index]">-->
-        <!--<i class="iconfont icon-zhankai"></i>展开-->
-        <!--</div>-->
-        <!--<div v-show="!toggleShowTag[index]">-->
-        <!--<i class="iconfont icon-shouqi"></i>收起-->
-        <!--</div>-->
-        <!--</div>-->
-        <div class="operation">
-          <div class="edit"><i class="iconfont icon-75bianji" @click.stop="goEdit(article.id)"></i></div>
-          <div class="delete" @click.stop="deleteArticle(article.id)"><i class="iconfont icon-shanchu"></i></div>
+    <div class="list-box">
+      <div class="article-list" v-for="(article,index) in personRecordList" key="article.id"
+           @click="goDetail(article.id)">
+        <div class="article-title">标题:<span style="margin-left: 5px">{{article.title |filterLength}}</span></div>
+        <div class="article-time">日期：{{article.createTime | dataFilter}}</div>
+        <div class="last-line">
+          <div class="operation">
+            <div class="edit"><i class="iconfont icon-75bianji" @click.stop="goEdit(article.id)"></i></div>
+            <div class="delete" @click.stop="deleteArticle(article.id)"><i class="iconfont icon-shanchu"></i></div>
+          </div>
         </div>
       </div>
     </div>
@@ -38,8 +30,7 @@
       return {
         id: this.$route.query.id,
         scrrenHeight: '',
-        personRecordList: [],
-        toggleShowTag: []
+        personRecordList: []
       };
     },
     created () {
@@ -58,7 +49,6 @@
           useLoadLayer: true,
           successCallback: function (data) {
             this.personRecordList = data.personRecordList;
-            this.toggleShowTag = new Array(this.personRecordList.length).fill(true);
           }.bind(this),
           errorCallback: function (data) {
             this.$router.go(-1);
@@ -85,11 +75,6 @@
       check(val){
         return val.replace(/\s/g, "").length > 100;
       },
-      toggle(index){
-        this.toggleShowTag[index] = !this.toggleShowTag[index];
-        this.toggleShowTag.push();
-        console.log(this.toggleShowTag[index]);
-      },
       goBack() {
         this.$router.go(-1);
       },
@@ -105,7 +90,7 @@
     },
     filters: {
       filterLength(val){
-        return val.length > 50 ? val.substring(0, 50) + '...' : val;
+        return val.length > 10 ? val.substring(0, 20) + '...' : val;
       },
       dataFilter(val){
         return dataFormate(new Date(val), 'yyyy年MM月dd日 hh:mm:ss');
@@ -115,7 +100,6 @@
 </script>
 <style scoped lang="less" rel="stylesheet/less">
   .person-article-list {
-
     color: #808080;
     .add {
       font-size: 30px;
@@ -126,39 +110,53 @@
         margin-left: 5px;
       }
     }
-    .article-list {
-      margin: 20px 30px 30px 30px;
-      background-color: #f3f4f8;
-      padding: 0 5px 0 5px;
-      border-bottom: 2px solid #8c8c8c;
-      &:last-child {
-        border-bottom: none;
-      }
-      .article-time {
-        text-align: right;
-      }
-      .article-body {
-        margin-left: 10px;
-      }
-      .toggleShow {
-        height: 20px;
-        white-space: nowrap;
-        overflow: hidden;
-        text-overflow: ellipsis;
-      }
-      .last-line {
-        margin-top: 10px;
-        margin-bottom: 10px;
-        .hidden {
-          i {
-            font-size: 12px;
-            margin-right: 5px;
-          }
+    .list-box {
+      margin: 0 30px 0 30px;
+      display: flex;
+      justify-content: space-between;
+      flex-wrap: wrap;
+      .article-list {
+        padding: 5px 5px 0 5px;
+        background-color: #f3f4f8;
+        border: 1px solid #8c8c8c;
+        width: 29%;
+        margin-top: 5px;
+        .article-title {
+          display: block;
+          white-space: nowrap;
+          overflow: hidden;
+          text-overflow: ellipsis;
         }
-        .operation {
-          display: flex;
-          .delete {
-            margin-left: 20px;
+        .article-time {
+          text-align: right;
+          display: block;
+          white-space: nowrap;
+          overflow: hidden;
+          text-overflow: ellipsis;
+        }
+        .article-body {
+          margin-left: 10px;
+        }
+        .toggleShow {
+          height: 20px;
+          white-space: nowrap;
+          overflow: hidden;
+          text-overflow: ellipsis;
+        }
+        .last-line {
+          margin-top: 10px;
+          margin-bottom: 10px;
+          .hidden {
+            i {
+              font-size: 12px;
+              margin-right: 5px;
+            }
+          }
+          .operation {
+            display: flex;
+            .delete {
+              margin-left: 20px;
+            }
           }
         }
       }
